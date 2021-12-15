0877ba4421de883211e28dbeb95a2e9c9a34ad56
Kevin Pratt <redhotkbp@hotmail.com>
2021-12-15 11:28:22 -0600

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,8 +25,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
     
     render() {
@@ -51,8 +51,8 @@ class CommentForm extends Component {
                                     </Control.select>
                             </Row>
                             <Row className="form-group">
-                                <Label htmlFor="yourName" md={3}>Your Name</Label>
-                                    <Control.text model=".yourName" id="yourName" name="yourName"
+                                <Label htmlFor="author" md={3}>Your Name</Label>
+                                    <Control.text model=".author" id="author" name="author"
                                         placeholder="Your Name"
                                         className="form-control"
                                         validators={{
@@ -61,7 +61,7 @@ class CommentForm extends Component {
                                          />
                                     <Errors 
                                         className="text-danger"
-                                        model=".yourName"
+                                        model=".author"
                                         show="touched"
                                         messages={{
                                             required: 'Required',
@@ -71,8 +71,8 @@ class CommentForm extends Component {
                                     />
                             </Row>
                             <Row className="form-group">
-                                <Label htmlFor="message" md={3}>Comment</Label>
-                                <Control.textarea model=".message" id="message" name="message"
+                                <Label htmlFor="comment" md={3}>Comment</Label>
+                                <Control.textarea model=".comment" id="comment" name="comment"
                                     rows="5"
                                     className="form-control" />
                              </Row>
@@ -105,7 +105,9 @@ class CommentForm extends Component {
                     </div>
                     <div className="row">
                         <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.comments}/>
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}    />
                     </div>
                 </div>
             );
@@ -132,7 +134,7 @@ class CommentForm extends Component {
         );
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if (comments != null) {
         const commentItem = comments.map((comment) => {
             return (     
@@ -149,7 +151,7 @@ class CommentForm extends Component {
                 <Card>
                     <h4>Comments</h4>
                     <ul className="list-unstyled">{commentItem}</ul>
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </Card>
             </div>
         );
