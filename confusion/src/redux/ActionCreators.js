5178ca1657844d94fb61ea465ff4011ebfb1efc9
Kevin Pratt <redhotkbp@hotmail.com>
2021-12-20 15:51:20 -0600

    Fetch from Server

---
+++ confusion/src/redux/ActionCreators.js
@@ -0,0 +1,73 @@
+import * as ActionTypes from './ActionTypes';
+import {DISHES} from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl';
+
+export const addComment = (dishId, rating, author, comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: {
+        dishId: dishId,
+        rating: rating,
+        author: author,
+        comment: comment
+    }
+});
+
+export const fetchDishes = () => (dishpatch) => {
+    dishpatch(dishesLoading(true));
+
+    return fetch(baseUrl + 'dishes')
+        .then(response => response.json())
+        .then(dishes => dishpatch(addDishes(dishes)));
+}
+
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
+});
+
+export const fetchComments = () => (dishpatch) => {
+    return fetch(baseUrl + 'comments')
+        .then(response => response.json())
+        .then(comments => dishpatch(addComments(comments)));
+}
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromos = () => (dishpatch) => {
+    dishpatch(promosLoading(true));
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => response.json())
+        .then(promos => dishpatch(addPromos(promos)));
+}
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
+});
